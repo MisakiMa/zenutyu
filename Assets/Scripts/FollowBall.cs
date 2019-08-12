a1c17780f2b081d90b4a95406e38ca4660f1f8bf
macoto1655 <mayu.bitte@gmail.com>
2019-08-12 20:24:10 +0900

    first commit

---
+++ Assets/Scripts/FollowBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBall : MonoBehaviour
+{
+    /// <summary>
+    /// ターゲットへの参照
+    /// </summary>
+    public Transform target;
+
+    private Vector3 offset; //相対座標
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //自分自身とtargetとの相対距離を求める
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().position = target.position + offset;
+    }
+}
