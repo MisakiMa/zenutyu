598504789c4554e44055d22e164d2e7079d0ab4b
ragesunTM <potirage@gmail.com>
2019-08-15 05:22:19 +0900

    Ball_TypeAとばすの追加

---
+++ Assets/Scripts/Ball_A.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_A : MonoBehaviour
+{
+    private Rigidbody ball = null;
+    Vector3 force = new Vector3 (0.0f,3.0f,0.0f);
+
+    public void Awake(){
+        this.ball = this.GetComponent<Rigidbody>();
+    }
+
+    void start(){
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClickBall(){
+        this.ball.AddForce(force,ForceMode.Impulse);
+    }
+}
